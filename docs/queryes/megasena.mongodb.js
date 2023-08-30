6ce5bf0e3391984711c403a4ac32f05c08e65b06
MrXcao <francisco.ramos@gmail.com>
2023-08-30 19:22:09 -0300

    doc with date

--- docs/queryes/megasena.mongodb.js
+++ docs/queryes/megasena.mongodb.js
@@ -7,7 +7,7 @@ const count = db.getCollection(collection).find({ }).count();
 console.log(`${collection} count ${count}`);
 
 db.getCollection(collection).aggregate([
-  { $project: { _id: 0, concurso: 1 } },
+  { $project: { _id: 0, concurso: 1, data: 1 } },
   {
     $sort: {
       concurso: -1,
