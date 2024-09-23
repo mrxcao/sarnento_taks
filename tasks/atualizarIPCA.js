dc0a6597665e8d9c9de3b3f9b48deab50ceb0d3a
Francisco Ramos <francisco.ramos@gmail.com>
2024-09-23 14:21:02 -0300

    schedule adjust

--- tasks/atualizarIPCA.js
+++ tasks/atualizarIPCA.js
@@ -3,7 +3,7 @@ const mod = require('../modules/ipca');
 const name = 'atualizaIPCA';
 module.exports = {
   name,
-  schedule: { hour: [1], minute: [0], dayOfWeek: [0, 1, 2, 3, 4, 5, 6, 7] },
+  schedule: { hour: [10], minute: [0], dayOfWeek: [0, 1, 2, 3, 4, 5, 6, 7] },
   callback: async () => {
     const debugMode = process.env.DEBUG === 'true';
     console.log(new Date(), name, 'inicio');
--- tasks/megasena.js
+++ tasks/megasena.js
@@ -3,7 +3,7 @@ const loterias = require('../modules/loterias');
 const name = 'megaSena';
 module.exports = {
   name,
-  schedule: { hour: [15, 16, 17], minute: [0], dayOfWeek: [0, 1, 2, 3, 4, 5, 6, 7] },
+  schedule: { hour: [9, 12, 16, 20], minute: [0], dayOfWeek: [0, 1, 2, 3, 4, 5, 6, 7] },
   callback: async () => {
     const debugMode = process.env.DEBUG === 'true';
     console.log(new Date(), name, 'inicio');
