c011f9f96835795ea8aa067b482c3ead8c15506e
Francisco Ramos <francisco.ramos@gmail.com>
2025-03-24 18:52:36 -0300

    ckode

--- tasks/megasena.js
+++ tasks/megasena.js
@@ -3,7 +3,11 @@ const loterias = require('../modules/loterias');
 const name = 'megaSena';
 module.exports = {
   name,
-  schedule: { hour: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 21, 22], minute: [0], dayOfWeek: [0, 1, 2, 3, 4, 5, 6, 7] },
+  schedule: {
+    hour: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 21, 22],
+    minute: [0],
+    dayOfWeek: [0, 1, 2, 3, 4, 5, 6, 7],
+  },
   callback: async () => {
     const debugMode = process.env.DEBUG === 'true';
     console.log(new Date(), name, 'inicio');
