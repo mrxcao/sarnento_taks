d7a96f065f8d94d11929a7e99b948338fab2e0db
Francisco Ramos <francisco.ramos@gmail.com>
2025-03-24 18:51:42 -0300

    teste

--- modules/loterias.js
+++ modules/loterias.js
@@ -32,7 +32,9 @@ const pegaQuantidade = (t, iniciaEm) => {
 
 const wait = async (page) => {
   // await page.waitForTimeout(3000); // Espera um pouco antes de pegar o HTML
-  await tools.delay(debugMode ? 0.1 : 3000);
+  console.log('1', 1);
+  await tools.delay(debugMode ? 0.1 : 300);
+  console.log('1', 2);
   const pageContent = await page.content();
   console.log(pageContent);
 };
